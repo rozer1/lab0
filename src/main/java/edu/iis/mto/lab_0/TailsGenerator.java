11ad9ec4fe41a058953a5c0fe02fb4c55513bdf0
Skorza.Tobiasz <209458@edu.p.lodz.pl>
2020-03-03 18:14:45 +0100

    Test passed

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -1,12 +1,27 @@
 package edu.iis.mto.lab_0;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+
+        List<String> list = new ArrayList<>();
+
+        if(value == null){
+            return Collections.emptyList();
+        }
+
+        for(int i=0;i<value.length(); i++){
+            list.add(String.valueOf(value.charAt(i)));
+        }
+
+        list.add("");
+        list.set(0,value);
+
+        return list;
     }
 
 }
